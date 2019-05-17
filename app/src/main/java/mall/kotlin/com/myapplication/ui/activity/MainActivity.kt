ea196715d792510a3b28904a21c29695ed0cf52c
幻优 <837639041@qq.com>
2019-05-17 19:09:16 +0800

    项目初始化

---
+++ app/src/main/java/mall/kotlin/com/myapplication/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package mall.kotlin.com.myapplication.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import mall.kotlin.com.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
