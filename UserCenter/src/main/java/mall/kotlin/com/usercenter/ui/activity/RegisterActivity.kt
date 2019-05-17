dad45fc029db9fe55b564e7e5d4b153c6e4919a9
幻优 <837639041@qq.com>
2019-05-17 20:58:06 +0800

    模块动态加载完成

---
+++ UserCenter/src/main/java/mall/kotlin/com/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package mall.kotlin.com.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import mall.kotlin.com.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
